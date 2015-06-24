8c173ffbb737d9a14d373c03cf4b5206f49f5283
Lan Xiao <lx223@users.noreply.github.com>
2015-06-24 07:45:58 +0100

    added icon38

---
+++ js/contextMenu.js
@@ -0,0 +1,79 @@
+(function(window){
+  'use strict';
+
+  var contexts = ["all"];
+  var menuIds = {
+    "saveAndClose" : "read-later",
+    "separator" : "separator",
+    "tabsList" : "tabs-list"
+  }
+
+  chrome.contextMenus.removeAll(function(){
+    console.log("all context menus removed...");
+  });
+
+  chrome.contextMenus.create({
+    "id" : menuIds.saveAndClose,
+    "title" : "Save and close tab",
+    "contexts" : contexts
+  })
+
+  chrome.contextMenus.create({
+    "id" : menuIds.separator,
+    "type" : "separator",
+    "contexts" : contexts
+  })
+
+  chrome.contextMenus.create({
+    "id" : menuIds.tabsList,
+    "title" : "Reading list",
+    "contexts" : contexts
+  }, function(){
+    if (chrome.runtime.lastError) {
+      console.log("Got expected error: " + chrome.runtime.lastError.message);
+    } else {
+      rlStorage.getAllTabs(function(tabs){
+        for (var key in tabs) {
+          if (tabs.hasOwnProperty(key)) {
+            chrome.contextMenus.create({
+              "id" : key,
+              "title": tabs[key],
+              "parentId": menuIds.tabsList
+            });
+          }
+        }
+      });
+      console.log("reading list context menu populated...");
+    }
+  })
+
+  var rlContextMenu = {
+    addToReadingList : function(menuId, menuTitle) {
+      chrome.contextMenus.create({
+        "id" : menuId,
+        "title": menuTitle,
+        "parentId": menuIds.tabsList
+      });
+    },
+
+    removeFromReadingList : function(menuId) {
+      chrome.contextMenus.remove(menuId, function(){
+        console.log("a context menu removed...");
+      });
+    }
+  };
+
+  chrome.contextMenus.onClicked.addListener(function(info, tab){
+    console.log("context menu onCliked event fired...");
+    if (info.menuItemId === menuIds.saveAndClose) {
+      rlUtils.saveAndCloseTab();
+    }
+
+    if (info.parentMenuItemId === menuIds.tabsList) {
+      rlUtils.createTab(info.menuItemId);
+    }
+  })
+
+  window.rlContextMenu = rlContextMenu;
+
+})(window);
