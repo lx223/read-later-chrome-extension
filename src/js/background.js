2937d9be1f1dfda0f632a6e82b618bc1ff27cc8f
Lan Xiao <lx223@users.noreply.github.com>
2015-07-07 16:26:25 +0100

    Fix event listener bug

--- src/js/background.js
+++ src/js/background.js
@@ -5,21 +5,20 @@
     rlStorage.init();
     rlUtils.init();
     rlContextMenu.init();
-    
+
     // wire up command
     chrome.commands.onCommand.addListener(function(command) {
       rlUtils.saveAndCloseCurrentTab();
     });
   }
+  initExtn();
 
   chrome.runtime.onStartup.addListener(function() {
     console.log('Starting browser...');
-    initExtn();
   });
 
   chrome.runtime.onInstalled.addListener(function(details) {
     console.log('Installed extension...');
-    initExtn();
   });
 
   chrome.tabs.onActivated.addListener(function(activeInfo) {
